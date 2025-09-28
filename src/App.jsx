6c58b97cadad45ed09144546930b3259670550d2
Kat <kat.m.deans@gmail.com>
2025-09-28 13:45:04 -0400

    Start the project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>My to do list</h1>
+      <div>
+        <h2>Finish FES</h2>
+        <button>DELETE</button>
+      </div>
+            <div>
+        <h2>Finish the interview section</h2>
+        <button>DELETE</button>
+      </div>
+            <div>
+        <h2>Land 100k job</h2>
+        <button>DELETE</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
